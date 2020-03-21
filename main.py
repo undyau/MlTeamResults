08f2474a4600f18efa3c26f2036ea12262072d8f
Andy Simpson <undyau@gmail.com>
2020-03-21 22:20:18 +1100

    Working cut - generates HTML page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import configparser
+from collections import defaultdict
 
 divisions = {}
 
@@ -40,12 +41,8 @@ class Division:
 class Match:
     def __init__(self, team1, team2):
         self.team1 = team1
-        self.team2 = team2
+        self.team2 = team2     
         
-    def getResult(self):
-        return self.team1.name + " " + self.team1.getScore() + " " + self.team2.name + " " + self.team2.getScore()
-        
-
 class Team:
     def __init__(self, name):
         self.name = name
@@ -63,11 +60,7 @@ class Team:
         if name in self.members:
             return self.members[name]
         else:
-            return None
-        
-    def setScore(self, name, score):
-        if self.hasMember(name):
-            self.members[name].setScore(score)
+            return None           
             
     def getStatus(self, name):
         if self.hasMember(name):
@@ -75,35 +68,40 @@ class Team:
         else:
             return None
             
-    def getTimeBehind(self, timeBehind):
-        if self.hasMember(name):
-            return self.members[name].timeBehind
-        else:
-            return None
-            
     def getScore(self):
         score = 0
-        for member in self.members:
+        for member in self.members.values():
             score = score + member.score
-            return score
+        return score
             
     def getPlayers(self):
         str =""
         for name in self.members:
             str = str + name + ","
-        return str
+        return str[:-1]
             
 
 class Person:
     def __init__(self, name):
         self.name = name
         self.score = 0
-        self.status = "OK"
-        self.timeBehind = 0
+        self.status = "DNS"
+        self.runningTime = -1
     
-    def setTime(self, timeBehind, status):
-        self.timeBehind = timeBehind
+    def setTime(self, runningTime, status):
         self.status = status
+        self.runningTime = int(runningTime)
+        
+    def textTime(self):
+        mins = self.runningTime//60
+        secs = self.runningTime - (60*mins)
+        return str(mins) + ":" + str(secs)
+        
+    def getResult(self):
+        if self.runningTime > 0:
+            return "<td>" + self.name + "</td><td>" + self.textTime() + "</td><td>" + str(self.score) + "</td>"
+        else:    
+            return "<td>" + self.name + "</td><td>" + self.status + "</td><td>" + str(self.score)+ "</td>"
 
 def is_number(s):
     try:
@@ -137,13 +135,44 @@ def read_teams():
                 team1.addMembers(config[div][normalise(team1.name)])
                 team2.addMembers(config[div][normalise(team2.name)])
             
-def print_teams():
+def print_results():
+    print("<html>")
+    print("<head>")
+    print('<link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Roboto&display=swap" rel="stylesheet">')
+    print('<style>')
+    print("body {font-family: 'Permanent+Marker', cursive; font-size: 48px; color:darkgreen}")
+    print('</style>')
+    print('</head>')
+    print('<body>')
     for name,div in divisions.items():
-        print (name)
+        print ("<H3>" + name + "</H3>")
+
         for match in div.matches:
-            print (match.team1.name + " v " + match.team2.name)
-            print (match.team1.name + ": " + match.team1.getPlayers())
-            print (match.team2.name + ": " + match.team2.getPlayers())            
+            print ("<p>" + match.team1.name + ": " + str(match.team1.getScore()) + " v " + match.team2.name + ": " + str(match.team2.getScore()) + "</p>")
+            print('<table><tr><th scope="col">Runner</th><th scope="col">Time</th><th scope="col">Points</th><th scope="col">Runner</th><th scope="col">Time</th><th scope="col">Points</th></tr>')
+            team1s = match.team1.getPlayers().split(',')
+            team2s = match.team2.getPlayers().split(',')
+            index = 0
+            while index < max(len(team1s),len(team2s)):
+                if index < len(team1s):
+                    mem = match.team1.findMember(team1s[index])
+                    if mem == None:
+                        print("No match for " + team1s[index])
+                    res1 = mem.getResult()
+                else:
+                    res1 = "n.a.  -  0"
+                if index < len(team2s):
+                    mem = match.team2.findMember(team2s[index])
+                    res2 = mem.getResult()
+                else:
+                    res2 = "n.a.  -  0"
+                print("<tr>" + res1 + res2 + "</tr>")
+                index = index + 1
+            print("</table>")
+    print('</body>')
+    print('</html>')
+
+         
 
 def process_person(DivNameText, PersonResult, ResultLookup):
     Person = PersonResult.find("Person")
@@ -166,12 +195,12 @@ def process_person(DivNameText, PersonResult, ResultLookup):
     if Status == None:
         return
     if Status.text == "OK":
-        TimeBehind = Result.find("TimeBehind")
-        if TimeBehind == None:
+        RunningTime = Result.find("Time")
+        if RunningTime == None:
             return
-        ResultLookup[Given.text + " " + Family.text] = TimeBehind.text
+        ResultLookup[Given.text + " " + Family.text] = (RunningTime.text, Status.text)
     else:
-        ResultLookup[Given.text + " " + Family.text] = Status.text
+        ResultLookup[Given.text + " " + Family.text] = ("-1","-1", Status.text)
         
              
 def process_division(DivNameText, ClassResult):
@@ -184,17 +213,31 @@ def process_division(DivNameText, ClassResult):
         if DivNameText in divisions.keys():
             member = divisions[DivNameText].findMember(personName)
             if member != None:
-                if not is_number(res):
-                    member.setTime(-1, res)
-                else:
-                    member.setTime(int(res), "OK")
-                print(personName + " - found result")
+                member.setTime(res[0], res[1])
         else:
             print(DivNameText + " is not a configured division")
+            
+# Calculate the scores for each runner
+    
+    for match in divisions[DivNameText].matches:
+        scorers = defaultdict(list)
+        for member in match.team1.members.values():
+            if member.runningTime > -1:
+                scorers[member.runningTime].append(member)
+        for member in match.team2.members.values():
+            if member.runningTime > -1:
+                scorers[member.runningTime].append(member)
+        score = 10
+        for key in sorted(scorers.keys()):
+            for member in scorers[key]:
+                member.score = score
+            score = score - len(scorers[key])
+        
 
+        
+        
 # Read the team info from INI file
 read_teams()
-print_teams()
 
 # Read the results from IOF3 XML
 input_file = "ResultsIOF3.xml"
@@ -222,7 +265,7 @@ for ClassResult in ClassResults:
         if (Name.text.startswith("Division ")):
             process_division(Name.text, ClassResult)
             
- 
+print_results()
 
 
 
